d9a3ad23683b6473f68b552ddcf17ae34321f833
Rommel Rico <me@rommelrico.com>
2017-10-24 16:06:28 -0700

    Initial commit

---
+++ LearningXcodeExtensions/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LearningXcodeExtensions
+//
+//  Created by Rommel Rico on 10/24/17.
+//  Copyright © 2017 Rommel Rico. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# LearningXcodeExtensions
+
+A simple app for me to learn how to create Xcode extensions.
+
