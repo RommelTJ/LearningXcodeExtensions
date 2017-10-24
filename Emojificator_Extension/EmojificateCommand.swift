04a00417f2c6bf69ed91717ef3e065de58df534e
Rommel Rico <me@rommelrico.com>
2017-10-24 16:25:29 -0700

    Logic for EmojificatorCommand Source Editor Extension

--- Emojificator_Extension/EmojificateCommand.swift
+++ Emojificator_Extension/EmojificateCommand.swift
@@ -11,10 +11,44 @@ import XcodeKit
 
 class EmojificateCommand: NSObject, XCSourceEditorCommand {
     
+    let asciiToEmojiMap = [
+        ":)" : "😀",
+        ";)" : "😉",
+        ":(" : "☹️"]
+    
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
-        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        let lines = invocation.buffer.lines
+        
+        for (index, line) in lines.enumerated() {
+            if let line = line as? String, replaceableItemsExist(in: line) {
+                lines[index] = replaceASCIIWithEmoji(in: line)
+            }
+        }
         
         completionHandler(nil)
     }
     
+}
+
+extension EmojificateCommand {
+    
+    /// Returns whether the string contains an item that can be converted into emoji
+    func replaceableItemsExist(in string: String) -> Bool {
+        for asciiItem in asciiToEmojiMap.keys {
+            if string.contains(asciiItem) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    /// Replaces any ASCII items with their emoji counterparts and returns the newly 'emojified' string
+    func replaceASCIIWithEmoji(in string: String) -> String {
+        var line = string
+        for asciiItem in asciiToEmojiMap.keys {
+            line = line.replacingOccurrences(of: asciiItem, with: asciiToEmojiMap[asciiItem]!)
+        }
+        return line
+    }
 }
